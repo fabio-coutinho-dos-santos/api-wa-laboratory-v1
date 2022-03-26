e1a267a9dbcc80c03ee2ebf2052f6c4ee1aee802
FabioSantCou <fabio.santcou@gmail.com>
2022-03-26 11:24:26 -0300

    fix style

--- src/Config/cors.js
+++ src/Config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req,resp,next){
-    resp.header('Access-Control-Allow-Origin','*')
-    resp.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    resp.header('Access-Control-Allow-HEADERS','Origin, X-Requested-With, Content-type, Accept, Authorization')
-    next()
+	resp.header("Access-Control-Allow-Origin","*")
+	resp.header("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, PATCH, DELETE")
+	resp.header("Access-Control-Allow-HEADERS","Origin, X-Requested-With, Content-type, Accept, Authorization")
+	next()
 }
--- src/Config/database.js
+++ src/Config/database.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env
 
-mongoose.Promise = global.Promise;
-const url =  process.env.DB_NAME ? `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin` : 'mongodb://localhost/db_gate'
+mongoose.Promise = global.Promise
+const url =  process.env.DB_NAME ? `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin` : "mongodb://localhost/db_gate"
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    bufferMaxEntries: 0,
-    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	autoIndex: false, // Don't build indexes
+	reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+	reconnectInterval: 500, // Reconnect every 500ms
+	poolSize: 10, // Maintain up to 10 socket connections
+	bufferMaxEntries: 0,
+	connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+	family: 4 // Use IPv4, skip trying IPv6
+}
 
-  module.exports = mongoose.createConnection(url, options, function (err, res) {
-    if (err) {
-    console.log ('ERROR connecting to: ' + url + '. ' + err);
-    } else {
-    console.log ('Succeeded connected to: ' + url);
+module.exports = mongoose.createConnection(url, options, function (err) {
+	if (err) {
+		console.log ("ERROR connecting to: " + url + ". " + err)
+	} else {
+		console.log ("Succeeded connected to: " + url)
     
-    }
-  }); 
+	}
+}) 
   
 mongoose.connect(url,options)
 
