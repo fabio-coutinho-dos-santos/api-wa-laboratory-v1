ab7904138b1d959e4256ba5ff4aa9b2153d20c9e
FabioSantCou <fabio.santcou@gmail.com>
2022-03-26 11:23:17 -0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./src/Config/server')
-require('./src/Config/database')
-require('./src/Config/routes')(server)
+const server = require("./src/Config/server")
+require("./src/Config/database")
+require("./src/Config/routes")(server)
 // require('./src/Mqtt/mqttService')
